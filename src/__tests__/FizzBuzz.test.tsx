97d7faf7a8253e1054164156358d828082b915cf
mixnuts07 <french0719@gmail.com>
2023-02-24 22:47:46 +0900

    first test not todo

--- src/__tests__/FizzBuzz.test.tsx
+++ src/__tests__/FizzBuzz.test.tsx
@@ -1,6 +1,14 @@
+// 仕様
 describe("FizzBuzz問題", () => {
-  test.todo('3の倍数を入力したら"Fizz"を出力する');
+  // テスト内容
+  test('3の倍数を入力したら"Fizz"を出力する', () => {
+    expect(Sample(3)).toBe(3);
+  });
   test.todo('5の倍数を入力したら"Buzz"を出力する');
   test.todo('3と5の倍数を入力したら"Fizz Buzz"を出力する');
   test.todo("それ以外の場合は入力値を出力する");
 });
+
+function Sample(num: Number) {
+  return num;
+}
